710882a431dbcfe22f29489c12ce2929ef18d267
vanisimov4 <144102084+vanisimov4@users.noreply.github.com>
2024-01-19 15:57:13 +0200

    start

--- js/task-1.js
+++
@@ -1,8 +0,0 @@
-const allItems = document.querySelectorAll('.item');
-console.log(`Number of categories: ${allItems.length}`);
-allItems.forEach(items => {
-  const title = items.querySelector('h2');
-  console.log(`Category: ${title.textContent}`);
-  const allLi = items.querySelectorAll('li');
-  console.log(`Elements: ${allLi.length}`);
-});
--- js/task-3.js
+++
@@ -1,7 +0,0 @@
-const textInput = document.querySelector('input#name-input');
-const output = document.querySelector('span#name-output');
-
-textInput.addEventListener('input', event => {
-  const textTrim = event.currentTarget.value.trim();
-  output.textContent = textTrim === '' ? 'Anonymous' : textTrim;
-});
